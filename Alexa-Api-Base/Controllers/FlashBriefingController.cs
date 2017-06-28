b3e2ecee6206893f23dfdc9411d4a37c9cffa68d
Naylorweb <jamie@naylorwebsites.co.uk>
2017-06-28 12:14:33 +0100

    Added flash breifing endpoint
    add the ability to return a flash briefing at api/FlashBriefing

---
+++ Alexa-Api-Base/Controllers/FlashBriefingController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AlexaBase.Models;
+
+namespace AlexaBase.Controllers
+{
+    public class FlashBriefingController : ApiController
+    {
+        // GET: FlashBriefing
+        public BriefingResponse GetFlashBriefing()
+        {
+            BriefingResponse briefing = new BriefingResponse();
+            briefing.uid = new Guid();
+            briefing.updateDate = DateTime.Now;
+            briefing.titleText = "Your custom flash briefing";
+            briefing.mainText = "This is your custom flash briefing response";
+            briefing.redirectionUrl = "https://github.com/DeadpoolPancakes/WebAPI-Alexa-Skill";
+            return briefing;
+        }
+    }
+}
---
+++ Alexa-Api-Base/Models/briefingResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlexaBase.Models
+{
+    public class BriefingResponse
+    {
+        public Guid uid { get; set; }
+        public DateTime updateDate { get; set; }
+        public string titleText { get; set; }
+        public string mainText { get; set; }
+        public string redirectionUrl { get; set; }
+    }
+}
